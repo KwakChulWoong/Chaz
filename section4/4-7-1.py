a60006c0dd48da1fcedba6318f3fcad87bfbc064
KwakChulWoong <socratechnic@naver.com>
2020-03-10 14:09:22 +0900

    hi

--- section4/4-7-1.py
+++ section4/4-7-1.py
@@ -1,1 +1,13 @@
 from pandas import Series
+
+# Series 선언
+series1=Series([92600,92400,92100,94300,92300])
+
+# 출력
+print(series1)
+
+# 총 개수
+print('count',series1.count())
+
+# 요약
+series1.describe()
